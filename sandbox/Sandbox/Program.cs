956a1007e429ff0a57f8c81c6ab868ced9be1b87
LizandroV <61571830+LizandroV@users.noreply.github.com>
2023-09-17 15:49:59 -0500

    first practice with console

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,6 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("There will not be a newline after this.");
         Console.WriteLine(".......");
+        int x;
+        x = 5;
+        Console.WriteLine(x);
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        if (x == 5){
+            Console.WriteLine("HIII");
+        }
+        Console.WriteLine($"My number is {x}");
     }
 }
