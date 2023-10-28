cc932245fb60322ec45e12627ec43796a4d2ecf2
LizandroV <61571830+LizandroV@users.noreply.github.com>
2023-10-28 10:56:49 -0500

    we did the "Prove 4"

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        WritingAssignment  A1 = new WritingAssignment ("Lizandro Vivanco", "European History", "The Causes of World War II");
+        System.Console.WriteLine(A1.GetSummary());
+        System.Console.WriteLine(A1.GetWritingInformation());
+
     }
 }
---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mindfulness_Program
+{
+    public class BreathingActivity : Activity
+    {
+        public BreathingActivity(string ActivityName, string Description) : base(ActivityName, Description)
+        {
+            
+        }
+
+        private void BreatheIn()
+        {
+            Console.Write("Breathe in...");
+            CountDown(4);
+            Console.WriteLine();
+        }
+
+        private void BreatheOut()
+        {
+             Console.Write("Now Breathe out...");
+            CountDown(6);
+            Console.WriteLine();
+        }
+
+        public void Run()
+        {
+            Prepare();
+            GetReady();
+
+            DateTime CurrentTime = DateTime.Now;
+            DateTime FutureTime = CurrentTime.AddSeconds(_duration);
+            do
+            {
+                Console.WriteLine();
+                BreatheIn();
+                BreatheOut();
+            } while (DateTime.Now < FutureTime);
+
+            Ending();
+        }
+    }
+}
